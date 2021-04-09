43e6f323648b8596479173acccad4fdfac39ab46
wouter1997 <woutervanakkeren@hotmail.com>
2021-04-09 18:18:18 +0200

    updated regression

--- src/analysis/regression.R
+++ src/analysis/regression.R
@@ -4,17 +4,19 @@ library(dplyr)
 library(fastDummies)
 library(dummies)
 options(scipen=999)
-data <- read.csv("data.csv")
+data <- read.csv("data/data.csv")
+
 
 #create dummy for neighbourhood
-data.new <- dummy.data.frame(data, names= c("neighbourhood"), sep= "_")
+data.new <- dummy.data.frame(data, names= c("neighbourhood", "month"), sep= "_")
+
 
 #create dummy for date
 data.new$date <- as.Date(data.new$date)
 data1 <- data.new %>% filter(date >= "2020-03-01")
 data2 <- data.new %>% filter(date < "2020-03-01")
-data1$stepdummy <- 1
-data2$stepdummy <- 0
+data1$coviddummy <- 1
+data2$coviddummy <- 0
 data.new <- rbind(data1, data2)
 
 #remove unwanted variables
@@ -27,11 +29,11 @@ mdl_reviews
 #outlier screening
 leverage_influence <- mdl_reviews %>%
   augment() %>%
-  select(num_reviews, month, leverage = .hat, cooks_dist = .cooksd) %>%
+  select(num_reviews, leverage = .hat, cooks_dist = .cooksd) %>%
   arrange(desc(cooks_dist)) %>%
   head()
 leverage_influence
-data.new.clean <- data.new[-c(1165, 1143, 1121, 1231, 1553, 1384),]
+data.new.clean <- data.new[-c(1165, 1143, 1121, 1231, 1553, 1187),]
 
 #run new lm with clean dataset
 mdl_reviews_clean <- lm(num_reviews ~ . , data=data.new.clean)
