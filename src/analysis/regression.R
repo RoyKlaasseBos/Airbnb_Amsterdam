388ca8c4b7467330702b9c1a44fa816ce399d65c
unknown <i.m.jansen@tilburguniversity.edu>
2021-04-09 20:46:13 +0200

    Regression update

--- src/analysis/regression.R
+++ src/analysis/regression.R
@@ -1,22 +1,15 @@
 ##Import the data
-
-df <- read.csv("../../data/data.csv")
-library(ggplot2)
-library(ggfortify)
-library(broom)
-library(dplyr)
-
-
-library(dummies)
 library(fastDummies)
-library(recipes)
-df$holidays <- ifelse(df$month == 12, 1, 0)
-df <- dummy_cols(df, select_columns = 'neighbourhood')
-df$dummyn <- factor(df$neighbourhood)
-df$dummyn[1:15]
-
+library(dplyr)
 library(stargazer)
+library(ggplot2)
+# DATA
+df <- read.csv("../../data/data.csv")
+df$holidays <- ifelse(df$month == 12, 1, 0)
+df$dummies <- dummy_cols(df, select_columns = 'neighbourhood')
 
+
+# Tabels text and html
 stargazer(df,
           title = "Figure 1:Stargazer Table",
           dep.var.caption = "Number of Reviews",  
@@ -27,6 +20,41 @@ stargazer(df,
           type="text",
           out="output.html"  
 )
+
+
+stargazer(df,
+          title = "Figure 1:Stargazer Table",
+          dep.var.caption = "Number of Reviews",  
+          dep.var.labels = "",  
+          covariate.labels = c("Month", "Neighbourhood", "Holiday"),  
+          column.labels = c("Full model"),
+          notes.label = "Significance levels",  
+          type="html",
+          out="output.html"  
+)
+
+
+lmmonth <- lm(month~num_reviews, data = df)
+lmholiday <- lm(holidays~num_reviews, data = df)
+summary(lmmonth)
+summary(lmholiday)
+
+
+stargazer(lmmonth, lmholiday, 
+          title = "Month and holidays  effect on number of reviews",
+          dep.var.caption = "Number of reviews", 
+          covariate.labels = c("Month", "Holiday"), 
+          notes.label = "significance levels", 
+          type = "text", 
+          out = "output.html")
+
+stargazer(lmmonth, lmholiday, 
+          title = "Month and holidays  effect on number of reviews",
+          dep.var.caption = "Number of reviews", 
+          covariate.labels = c("Month", "Holiday"), 
+          notes.label = "significance levels", 
+          type = "html", 
+          out = "output.html")
 
 library(ggplot2)
 #Regression month and reviews
@@ -50,8 +78,9 @@ ggplot(df, aes(x = holidays, y = num_reviews)) +
   ggtitle("Figure 2: Linear trend between IV and DV") +
   scale_colour_manual(name="Legend", values=c("red", "blue"))
 
+
 #Regression neighborhoods and reviews
-ggplot(df, aes(x = dummyn, y = num_reviews)) +
+ggplot(df, aes(x = neighbourhood, y = num_reviews)) +
   geom_point(alpha = 0.5) +  
   geom_smooth(method = "lm", se = FALSE, aes(color="Full model")) +
   geom_smooth(method = "lm", se = FALSE, data = df)  +
