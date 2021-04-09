2dfa25821e38e5a5eb277c2e81fa1364d91e6af8
Anouk Heemskerk <anoukheemskerk@gmail.com>
2021-04-09 14:27:25 +0200

    update regression code

--- src/analysis/regression.R
+++ src/analysis/regression.R
@@ -1,1 +1,35 @@
-# your R code goes here!
+### load packages -------------------------------------------------------------
+#install.packages('fastDummies')  uncomment if you dont have it yet
+library(fastDummies)
+library(dplyr)
+library(stargazer)
+
+### load data -----------------------------------------------------------------
+data <- read.csv('../../data/data.csv')
+
+### transform data ------------------------------------------------------------
+# add trend variable for the month
+data$trend <- data$month  # month already shows a number
+
+# add dummy variables for the various neighborhoods
+data <- dummy_cols(data, select_columns = 'neighbourhood')
+
+# transform date to date format
+data$date <- as.Date(data$date)
+
+# add step dummy for COVID pandemic
+data <- data %>%
+  mutate(data, dummy_covid = ifelse(date >= '2020-03-01', 1, 0))
+
+### apply regression ----------------------------------------------------------
+regression_model <- lm(num_reviews ~ . - X - year - month - neighbourhood -
+                         num_reviews - date, data=data)
+
+### create directories -------------------------------------------------------
+dir.create('../../gen')
+dir.create('../../gen/analysis')
+
+### output regression html ----------------------------------------------------
+stargazer(regression_model, type='html', title='Regression Analysis',
+          dep.var.labels="Number of Reviews", notes.label="",
+          out='../../gen/analysis/regression.html')
